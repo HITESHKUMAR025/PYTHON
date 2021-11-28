fc033d89f2634245eebf6c4f0fea2c380a6f7a5a
HITESH KUMAR <92049056+HITESHKUMAR025@users.noreply.github.com>
2021-11-28 21:32:40 +0530

    introduction python
    Why Python/
    Drawbacks of Python/
    Platform Independent Language/
    Data Driven Domains:/
    Other Domains:

---
+++ PythonIntro28nov.py
@@ -0,0 +1,93 @@
+"""Python
+There is no negative marking in our class
+"""
+"""
+Why Python:
+1. Easy Programming Language at Basic Level:
+User Friendly Lang
+2. Open Source.
+3. Interpreter based language
+4. Platform Independent Language
+5. Modular Approach of Programming 
+6. Python support OOPS
+7. Very Flexible Language
+8. Big set of standard libraries: 
+2.35 Lakhs+ Libraries: Each library is having
+100 to 50k set of functions, methods or classes.
+9. Used in Versatile Domains.
+10. Dynamic Data Type Definition 
+11. Dynamic Memory Allocation.
+Many more reasons to go.
+
+
+Drawbacks of Python:
+1. Very Slow Language
+2. To much memory consumption
+"""
+
+# print("hitesh")
+# print("hello")
+# pr("abc")
+# print("Welcome")
+# print("1234")
+
+"""
+Platform Independent Language:
+
+Portability: WORA: If you write your code on one
+OS and if the same code without any change can 
+be executed on any other OS then we say our
+language/technology/code is portable.
+
+Platform Independent: CORA: Compile Once Run
+Anywhere. If you write your code on one
+OS, and compile the code, after compilation, if
+ the compiled code can be executed on any 
+ other OS then we say our language/technology/
+ code is platform independent.
+ 
+"""
+
+# a,b=5,7
+# a,b=b,a
+# print(a,b)
+
+"""
+Community Based Learning
+
+Data Driven Domains:
+Data Analytics: Data Analyst
+Machine Learning
+Deep Learning
+AI
+Image Processing
+Natural Language Processing (NLP)
+
+Other Domains:
+Full Stack Development: Django + Web Designing
+Mobile Apps
+
+AI: Python + DA with Stats + ML + DL
+
+More Jobs for Full Stack than DS in India for Freshers
+
+Any student or professional from any background can learn Python.
+
+Learn Python The Hard Way.
+
+Lecture are getting being recorded: 
+Recorded Lecture Should reach at Portal max in 24 to 36 Hours. 
+
+
+Python:
+
+
+"""
+
+"""Data Science = Python +DA + ML 
+DS=DA+ ML
+
+ML: 
+
+
+"""
