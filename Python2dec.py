02cc35ee5f496211c4062cbf856719887d7e198f
HITESH KUMAR <92049056+HITESHKUMAR025@users.noreply.github.com>
2021-12-02 22:19:23 +0530

    LIST

---
+++ Python2dec.py
@@ -0,0 +1,37 @@
+"""Python Continue"""
+# L1=[22,33,44,55]
+# print(L1.index(44))
+
+# #New Program
+# x=5
+# if(x>5):
+#     pass
+# else:
+#     print("Ok")
+
+# #New Program
+# L1=[22,33,44]
+# L1.extend([55,66,77,88,99])
+# print(L1)
+# L1=[22,33,44]
+# L1.append([55,66,77,88,99])
+# print(L1)
+
+# #New Program
+# L1=[22,33,44,55,44]
+# print(L1.count(44))
+
+# #New Program
+# id=[10,20,30,40]
+# name=["hite","Anil","Amit","Sonu"]
+# age=[39,41,45,55]
+# i=30
+# index=id.index(i)
+# print(index)
+
+#New Program
+age=[33,44,12,19]
+age[2]=55
+print(age)
+
+
